759fe286642daa0a8ea0414d145dc87d59795bd1
Ümit KOÇ <49067234+umitkoc@users.noreply.github.com>
2020-08-30 22:54:58 +0300

    Add project files.

---
+++ Algoritma2/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma2
+{
+    public class Class1
+    {
+        public static void Main(string args)
+        {
+            Console.WriteLine("Merhaba Dünya");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Deneme1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deneme1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random rd = new Random();
+            int []dizi = new int[20];
+            int i = 0;
+            int sayi,j;
+            while (i < dizi.Length)
+            {
+                sayi = rd.Next(21);
+                j = 0;
+                while (j < dizi.Length)
+                {
+                    if (dizi[j] == sayi)
+                    {
+                        sayi = rd.Next(20);
+                        j = -1;
+                    }
+                    j++;
+                }
+                    dizi[i] = sayi;
+                    Console.Write($" {dizi[i]} ");
+                    i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
